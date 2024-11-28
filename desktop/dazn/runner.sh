a8f82be06523b0bd9f4782cc7c3501bd411c2f5e
Giorgio Daniele <giorgiodaniele15@gmail.com>
2024-11-28 15:17:34 +0100

    Introduction of autocompletion of sudo commands

--- desktop/dazn/runner.sh
+++ desktop/dazn/runner.sh
@@ -6,17 +6,24 @@ bandwidth_rates=(1500 3000 4500 6000 7500 50000)
 # Define the path to wondersharper
 wondershaper=wondershaper/wondershaper
 
+# Insert the password here for enabling wondershaper
+password=""
+
 # Define the network interface
 interface=enp1s0
 
 for rate in "${bandwidth_rates[@]}"; do
 
     # Invoke wondersharper to remove any previous configuration
-    $wondersharper -ca $interface
+    echo "$password" | sudo -S -k "$wondershaper" -ca "$interface"
 
     # Invoke wondersharper to set new bandwidth configuration
-    $wondersharper -a $interface -u $rate -d $rate
+    echo "$password" | sudo -S -k "$wondersharper" -a "$interface" -u "$rate" -d "$rate"
 
     # Invoke Streambot here
+    # TODO
 
 done
+
+# Remove any previous configuration before exiting
+echo "$password" | sudo -S -k "$wondershaper" -ca "$interface"
