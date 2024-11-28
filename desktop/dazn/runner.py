f0ae9e4a6e785ed2284d1715d29708a133da58fa
Giorgio Daniele <giorgiodaniele15@gmail.com>
2024-11-28 17:35:00 +0100

    Introduction of wrapper for nodejs

--- desktop/dazn/runner.py
+++ desktop/dazn/runner.py
@@ -3,39 +3,37 @@ import time
 
 bandwidth_rates  = [1500, 3000, 4500, 6000, 7500, 50000]
 wondershaper     = "wondershaper/wondershaper"
-password         = "user"
 interface        = "enp1s0"
 
-def run_command(command: list[str], password: str):
+def run_command(command: list[str]):
     process = subprocess.Popen(
         command, 
-        stdin=subprocess.PIPE, 
         stdout=subprocess.PIPE, 
         stderr=subprocess.PIPE, 
         text=True
     )
-    stdout, stderr = process.communicate(input=f"{password}\n")
+    stdout, stderr = process.communicate()
     return stdout, stderr
 
 def run_node_script(source_file: str):
     command = ["node", source_file]
-    stdout, stderr = run_command(command, password)
+    stdout, stderr = run_command(command)
     print(f"Running Node.js script {source_file}: {stdout}, {stderr}")
 
 for rate in bandwidth_rates:
-    command = [wondershaper, "-c", "-a", interface]
-    stdout, stderr = run_command(command, password)
+    command = ["sudo", wondershaper, "-c", "-a", interface]
+    stdout, stderr = run_command(command)
     print(f"Config removal for rate {rate}: {stdout}, {stderr}")
 
-    command = [wondershaper, "-a", interface, "-u", str(rate), "-d", str(rate)]
-    stdout, stderr = run_command(command, password)
+    command = ["sudo", wondershaper, "-a", interface, "-u", str(rate), "-d", str(rate)]
+    stdout, stderr = run_command(command)
     print(f"Setting new config for rate {rate}: {stdout}, {stderr}")
 
-    run_node_script("source.js")
+    run_node_script("scraping.js")
 
     print(f"Waiting for 5 seconds before setting the next rate...")
     time.sleep(5)
 
-command = [wondershaper, "-ca", interface]
-stdout, stderr = run_command(command, password)
+command = ["sudo", wondershaper, "-c", "-a", interface]
+stdout, stderr = run_command(command)
 print(f"Final config removal: {stdout}, {stderr}")
